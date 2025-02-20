c7a3dbb8493b9ff7239ef416c864990b3d342d48
Blazzzeee <legendgamer1007@gmail.com>
2025-02-20 15:30:59 +0530

    Added lsp and git support to lualine

--- nvim/lua/plugins/completions.lua
+++ nvim/lua/plugins/completions.lua
@@ -69,11 +69,11 @@ return {
                     }
                 },
                 sources = cmp.config.sources({
-                    { name = "nvim_lsp" },
-                    { name = "luasnip" },
+                    { name = "nvim_lsp", max_item_count = 5 },
+                    { name = "luasnip",  max_item_count = 5 },
                 }, {
                     { name = "buffer", keyword_length = 5, max_item_count = 5 },
-                    { name = "path", },
+                    { name = "path",   max_item_count = 5 },
                 }),
             })
 
--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,52 +1,100 @@
 return {
-	'nvim-lualine/lualine.nvim',
-	dependencies = { 'nvim-tree/nvim-web-devicons' },
-	config = function()
-		require('lualine').setup {
-			options = {
-				icons_enabled = true,
-				theme = 'auto',
-				component_separators = { left = '', right = '' },
-				section_separators = { left = '', right = '' },
-				disabled_filetypes = {
-					statusline = { "dashboard", "lazy", "alpha" },
-					winbar = {},
-				},
-				ignore_focus = {},
-				always_divide_middle = true,
-				globalstatus = false,
-				refresh = {
-					statusline = 1000,
-					tabline = 1000,
-					winbar = 1000,
-				},
-			},
-			sections = {
-				lualine_a = { 'mode' },
-				lualine_b = {
-					{
-						symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' },
-						sources = { "nvim_diagnostic" },
-						'diagnostics',
-					},
-				},
-				lualine_c = { { 'filename', path = 1 } },
-				lualine_x = {},
-				lualine_y = { '' },
-				lualine_z = { 'filetype' },
-			},
-			inactive_sections = {
-				lualine_a = {},
-				lualine_b = {},
-				lualine_c = { 'filename' },
-				lualine_x = { 'location' },
-				lualine_y = {},
-				lualine_z = {},
-			},
-			tabline = {},
-			winbar = {},
-			inactive_winbar = {},
-			extensions = { 'fugitive' },
-		}
-	end
+    "nvim-lualine/lualine.nvim",
+    config = function()
+        -- Custom function to display LSP client names for the current buffer
+        local function lsp_client()
+            local msg = "No Active LSP"
+            local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+            local clients = vim.lsp.get_active_clients()
+            if not clients or vim.tbl_isempty(clients) then
+                return msg
+            end
+
+            local client_names = {}
+            for _, client in ipairs(clients) do
+                local filetypes = client.config.filetypes
+                if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                    table.insert(client_names, client.name)
+                end
+            end
+
+            if #client_names > 0 then
+                return table.concat(client_names, ", ")
+            else
+                return msg
+            end
+        end
+
+        local M = {}
+        M.theme = function()
+            local colors = {
+                darkgray = "#16161d",
+                gray = "#727169",
+                innerbg = nil,
+                outerbg = "#16161D",
+                normal = "#7e9cd8",
+                insert = "#98bb6c",
+                visual = "#ffa066",
+                replace = "#e46876",
+                command = "#e6c384",
+            }
+            return {
+                inactive = {
+                    a = { fg = colors.gray, bg = colors.outerbg, gui = "bold" },
+                    b = { fg = colors.gray, bg = colors.outerbg },
+                    c = { fg = colors.gray, bg = colors.innerbg },
+                },
+                visual = {
+                    a = { fg = colors.darkgray, bg = colors.visual, gui = "bold" },
+                    b = { fg = colors.gray, bg = colors.outerbg },
+                    c = { fg = colors.gray, bg = colors.innerbg },
+                },
+                replace = {
+                    a = { fg = colors.darkgray, bg = colors.replace, gui = "bold" },
+                    b = { fg = colors.gray, bg = colors.outerbg },
+                    c = { fg = colors.gray, bg = colors.innerbg },
+                },
+                normal = {
+                    a = { fg = colors.darkgray, bg = colors.normal, gui = "bold" },
+                    b = { fg = colors.gray, bg = colors.outerbg },
+                    c = { fg = colors.gray, bg = colors.innerbg },
+                },
+                insert = {
+                    a = { fg = colors.darkgray, bg = colors.insert, gui = "bold" },
+                    b = { fg = colors.gray, bg = colors.outerbg },
+                    c = { fg = colors.gray, bg = colors.innerbg },
+                },
+                command = {
+                    a = { fg = colors.darkgray, bg = colors.command, gui = "bold" },
+                    b = { fg = colors.gray, bg = colors.outerbg },
+                    c = { fg = colors.gray, bg = colors.innerbg },
+                },
+            }
+        end
+
+        local custom_material = M
+        require("lualine").setup {
+            options = {
+                theme = custom_material.theme(),
+                component_separators = { left = '', right = '' },
+                section_separators = { left = '', right = '' },
+                disabled_filetypes = {},
+                globalstatus = false,
+            },
+            sections = {
+                lualine_a = { "mode" },
+                lualine_b = { "branch" },
+                lualine_c = {},
+                lualine_x = { "filetype" },
+                lualine_y = { "filename" },
+                lualine_z = { lsp_client },
+            },
+            inactive_sections = {
+            },
+            tabline = {},
+            extensions = { "fugitive", "oil", "mason", "lazy" },
+
+
+        }
+    end,
 }
